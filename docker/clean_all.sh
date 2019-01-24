cfbc26be5943f6236a3365cced6d8af0d341821b
Marco Lancini <info@marcolancini.it>
2019-01-24 20:40:44 +0000

    [ANSIBLE] Update role kali-cloud

--- docker/clean_all.sh
+++ docker/clean_all.sh
@@ -1,6 +1,12 @@
 # Remove all exited containers
 sudo docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm
 
+# Remove untagged images
+docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
+
+# Remove dangling images
+docker rmi $(docker images --quiet --filter "dangling=true")
+
 # Remove all unused or dangling images, containers, volumes, and networks
 docker system prune -a
 
