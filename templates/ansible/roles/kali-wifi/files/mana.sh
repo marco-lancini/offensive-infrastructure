bf082ec9f5c60c33647ab0cf62fa1f13545b3701
marco-lancini <info@marcolancini.it>
2020-10-25 16:13:05 +0000

    [ADMIN] Cleanup old resources

--- templates/ansible/roles/kali-wifi/files/mana.sh
+++
@@ -1,9 +0,0 @@
-#--- Disable profile
-a2dissite 000-mana-toolkit; a2ensite 000-default
-#--- Setup alias
-file=~/.bash_aliases
-grep -q '^## mana-toolkit' "${file}" 2>/dev/null \
-  || (echo '## mana-toolkit\nalias mana-toolkit-start="a2ensite 000-mana-toolkit;a2dissite 000-default; systemctl restart apache2"' >> "${file}" \
-    && echo 'alias mana-toolkit-stop="a2dissite 000-mana-toolkit; a2ensite 000-default; systemctl restart apache2"\n' >> "${file}" )
-#--- Apply new alias
-source "${file}" || source ~/.zshrc
